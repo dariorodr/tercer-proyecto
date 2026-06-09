3015ffea4fae8aeb48739d6ac8d4cec911c98794
Dario Rodriguez <dariodis97@gmail.com>
2026-06-09 00:12:56 -0300

    cambios en settings.py

--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -88,7 +88,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 
 # Archivos multimedia
 CLOUDINARY_STORAGE = {
